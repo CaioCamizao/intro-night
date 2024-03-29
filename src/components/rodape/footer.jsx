f48a8006e2d9b0cef7f430b7a841d07fe3934f3f
Caio Camizão <caio_quimica2013@hotmail.com>
2024-03-28 21:41:25 -0300

    WIP: increment footer with database apps

--- src/components/rodape/footer.jsx
+++ src/components/rodape/footer.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import './footer.css';
+import { FaInstagram, FaFacebookF, FaTwitter } from 'react-icons/fa';
 
 function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>Seu conteúdo de rodapé aqui.</p>
+        <div className="social-icons">
+          <a href="https://www.instagram.com"><FaInstagram /></a>
+          <a href="https://www.facebook.com"><FaFacebookF /></a>
+          <a href="https://www.twitter.com"><FaTwitter /></a>
+        </div>
+        <p>Todos os direitos reservados</p>
       </div>
     </footer>
   );
 }
 
 export default Footer;
+
